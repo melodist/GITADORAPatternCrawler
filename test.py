7808657427ae889e3181c4ad676ce56e396432c0
melodist <melt.ep00@gmail.com>
2022-09-25 23:54:46 +0900

    Feat: 크롤러 테스트 코드 추가

--- test.py
+++ test.py
@@ -9,6 +9,14 @@ class TestStringMethods(unittest.TestCase):
         request = requests.get('https://google.com')
         self.assertEqual(request.status_code, 200)
 
+    def test_request_crawl(self):
+        session_id = input()
+        url = 'https://p.eagate.573.jp/game/gfdm/gitadora_highvoltage/p/playdata/music_detail.html?' \
+              'gtype=gf&cat=&sid=2&index=2&page=1'
+        request = requests.get(url=url, cookies={'M573SSID': session_id})
+        print(request.text)
+        self.assertRegex(request.text, r'1/n')
+
 
 if __name__ == '__main__':
     unittest.main()
