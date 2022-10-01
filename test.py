9cf93b33787bc418bd6c403a3bea3b52259832dd
melodist <melt.ep00@gmail.com>
2022-10-01 22:56:28 +0900

    Feat: BeautifulSoup 테스트 코드 추가

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import unittest
 
 import requests
+from bs4 import BeautifulSoup
+
+
+def request(session_id, url):
+    return requests.get(url=url, cookies={'M573SSID': session_id})
 
 
 class TestStringMethods(unittest.TestCase):
@@ -10,12 +15,22 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(request.status_code, 200)
 
     def test_request_crawl(self):
-        session_id = input()
+        session_id = '36565579-b2bd-456f-a32f-eb3973bdf34d'
         url = 'https://p.eagate.573.jp/game/gfdm/gitadora_highvoltage/p/playdata/music_detail.html?' \
               'gtype=gf&cat=&sid=2&index=2&page=1'
         request = requests.get(url=url, cookies={'M573SSID': session_id})
         print(request.text)
         self.assertRegex(request.text, r'1/n')
+
+    def test_beautiful_soup(self):
+        session_id = '36565579-b2bd-456f-a32f-eb3973bdf34d'
+        url = 'https://p.eagate.573.jp/game/gfdm/gitadora_highvoltage/p/playdata/music_detail.html?' \
+              'gtype=gf&cat=&sid=2&index=2&page=1'
+        r = request(session_id, url)
+        bs = BeautifulSoup(r.text, 'html.parser')
+        tag = bs.find('div', attrs={'class': 'live_title'})
+        [print(s) for s in tag.strings]
+        self.assertIn("1/n", tag.strings)
 
 
 if __name__ == '__main__':
